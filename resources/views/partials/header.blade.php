96c8d98de446b0f77ada47fd5dce0419f4f23c28
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-08 17:18:05 +0100

    add nav bar and imgs

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,25 @@
 <header class="">
-    <h1>
-        header
-    </h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a href="#" class="">
+                <img src="{{Vite:: asset('resources/assets/imgs/dc-logo.png')}}" alt="" class="w-50 ms-4">
+            </a>
+            <div class=" navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
